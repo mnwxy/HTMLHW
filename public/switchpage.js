e58e0115fc9f92f22bd0bb2d6f60fae5acae8965
mnwxy <william.k.tao@gmail.com>
2020-11-10 11:16:57 -0800

    added game

--- public/switchpage.js
+++ public/switchpage.js
@@ -1,15 +1,10 @@
 var pageNumber = 2;
 const NUM_PAGES = 2;
-pages = new Array(NUM_PAGES);
-for(i = 1; i <= NUM_PAGES; i++)
-{
-  pages[i] = FileReader.readAsText("page" + i + ".html");
-}
 
 document.getElementById("rightArrow")
   .addEventListener("click", function(event){
     pageNumber = (pageNumber + 1) % NUM_PAGES;
-    document.getElementById("test").innerHTML = pages[i];
+    document.getElementById("page").setAttribute("src", "page" + (pageNumber+1) + ".html");
   });
   document.getElementById("leftArrow")
     .addEventListener("click", function(event){
