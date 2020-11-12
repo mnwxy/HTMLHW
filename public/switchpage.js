cd1f6c308387678082c92ea92205d43405619b07
mnwxy <william.k.tao@gmail.com>
2020-11-12 09:29:55 -0800

    edited styling, migrated away from iframes, added jquery

--- public/switchpage.js
+++ public/switchpage.js
@@ -1,13 +1,31 @@
 var pageNumber = 2;
 const NUM_PAGES = 2;
+function unloadContent()
+{
+  $("#page").html("");
+}
 
-document.getElementById("rightArrow")
-  .addEventListener("click", function(event){
-    pageNumber = (pageNumber + 1) % NUM_PAGES;
-    document.getElementById("page").setAttribute("src", "page" + (pageNumber+1) + ".html");
-  });
-  document.getElementById("leftArrow")
-    .addEventListener("click", function(event){
-      pageNumber = (pageNumber - 1) % NUM_PAGES;
-      document.getElementById("page").setAttribute("src", "page" + (pageNumber+1) + ".html");
-    });
+function loadContent(result)
+{
+  unloadContent();
+  $("#page").html(result);
+}
+function changePage(input)
+{
+  pageNumber = (pageNumber + parseInt(input.data.direction)) % NUM_PAGES;
+  unloadContent();
+  $.ajax({url: "/public/page" + (pageNumber+parseInt(input.data.direction)) + ".html", success: loadContent});
+}
+
+$("#rightArrow").click({direction: 1},changePage);
+
+// document.getElementById("rightArrow")
+//   .addEventListener("click", function(event){
+//     pageNumber = (pageNumber + 1) % NUM_PAGES;
+//     document.getElementById("page").setAttribute("src", "page" + (pageNumber+1) + ".html");
+//   });
+//   document.getElementById("leftArrow")
+//     .addEventListener("click", function(event){
+//       pageNumber = (pageNumber - 1) % NUM_PAGES;
+//       document.getElementById("page").setAttribute("src", "page" + (pageNumber+1) + ".html");
+//     });
